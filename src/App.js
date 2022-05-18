043d1307e65b52e96dc33f16c058de3b35e45c02
DWPongWin <49766063+rwmahrra@users.noreply.github.com>
2022-05-18 13:03:56 -0500

    adding canvas

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 import {Canvas} from '@react-three/fiber'
-import {AIOpponent} from './Models/AIOpponent'
+import { OrbitControls } from '@react-three/drei';
+
+import AIOpponent from './Models/AIOpponent.js'
 
 function App() {
   return (
-    <div id="canvas-container">
-      <Canvas>
-        <ambientLight intensity={0.1} />
-        <directionalLight color="red" position={[0, 0, 5]} />
-        <AIOpponent />
+      <Canvas mode="concurrent">
+        <OrbitControls></OrbitControls>
+        <ambientLight intensity={0.3}></ambientLight>
+        <spotLight position={[0,0,5]} angle={0.4} intensity={1.0} />
+        <AIOpponent></AIOpponent>
       </Canvas>
-    </div>
   );
 }
 
