5c0700f681c97a5f3ce940b000b17e720f64b591
DWPongWin <49766063+rwmahrra@users.noreply.github.com>
2022-05-19 16:02:45 -0500

    exploded cofig to add mqtt library, added animations

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 import {Canvas} from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, Sky, Environment } from '@react-three/drei';
 
 import AIOpponent from './Models/AIOpponent.js'
 
 function App() {
   return (
-      <Canvas mode="concurrent">
+      <Canvas mode="concurrent" camera={{position: [0,0,2.5]}}>
         <OrbitControls></OrbitControls>
-        <ambientLight intensity={0.3}></ambientLight>
+        <ambientLight intensity={0.5}></ambientLight>
         <spotLight position={[0,0,5]} angle={0.4} intensity={1.0} />
         <AIOpponent></AIOpponent>
+        <Sky />
       </Canvas>
   );
 }
